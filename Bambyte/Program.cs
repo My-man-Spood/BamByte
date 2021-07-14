5e0c5e7450e2cef4b61980c00b5126293197229b
jeremie <ownedstuff@hotmail.fr>
2021-07-14 16:51:33 -0400

    Create solution

---
+++ Bambyte/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bambyte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
